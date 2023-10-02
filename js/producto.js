ca4dfc43f507949157268dcf4cc881edb8ae339e
ayelenl <aielatronico@gmail.com>
2023-10-02 20:37:42 -0300

    agregue cambios de estilo, funcion para estrellas

--- js/producto.js
+++ js/producto.js
@@ -1,6 +1,16 @@
 const seleccionado = JSON.parse(localStorage.getItem("productos"));
 const mostrarInfo = document.querySelector("#informacion-producto");
 
+function convertirAEstrellas(estrellas) {
+  const estrellasLlenas = estrellas.length;
+  const estrellasVacias = 5 - estrellasLlenas;
+
+  const estrellasHTML = '<span class="estrella-llena">★</span>'.repeat(estrellasLlenas) +
+      '<span class="estrella-vacia">☆</span>'.repeat(estrellasVacias);
+
+  return `<p class="puntuacion-estrellas">${estrellasHTML}</p>`;
+}
+
 seleccionado.forEach((productoselec) => {
   let contenido = document.createElement("div");
   contenido.innerHTML = `
@@ -9,7 +19,7 @@ seleccionado.forEach((productoselec) => {
             <img src="img/${productoselec.imagen}">
             <p>${productoselec.descripcion}</p>
             <p class="price-product">$${productoselec.precio}</p>
-            <p>${productoselec.estrellas}</p>
+            <p>${convertirAEstrellas(productoselec.estrellas)}</p>
         </div>
         `;
 
